67149bb3727d2b56a06bd55dea85b08fc0808388
第四维 <oncwnuJXA1XBgjZaB0qBXUllo7N0@git.weixin.qq.com>
2020-04-26 11:23:27 +0800

    第一次提交

---
+++ app/src/main/java/com/cf/xpermission/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.cf.xpermission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *@作者:陈飞
+ *@说明:申请类主页
+ *@时间:2020/4/26 11:05
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
